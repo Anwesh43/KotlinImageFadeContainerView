e1415c2bf79a37c7cd5db758eb0d589a484e6ab1
Anwesh43 <anweshthecool0@gmail.com>
2018-01-18 00:34:09 +0530

    adding ImageFadeContainerView class

--- imagefadecontainerview/src/main/java/ui/anwesome/com/imagefadecontainerview/ImageFadeContainerView.kt
+++ imagefadecontainerview/src/main/java/ui/anwesome/com/imagefadecontainerview/ImageFadeContainerView.kt
@@ -3,3 +3,19 @@ package ui.anwesome.com.imagefadecontainerview
 /**
  * Created by anweshmishra on 18/01/18.
  */
+import android.view.*
+import android.content.*
+import android.graphics.*
+class ImageFadeContainerView(ctx:Context):View(ctx) {
+    override fun onDraw(canvas:Canvas) {
+
+    }
+    override fun onTouchEvent(event:MotionEvent):Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
