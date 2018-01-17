c97aa92060d2fc6b9e7dadcb69f1901e69759025
Anwesh43 <anweshthecool0@gmail.com>
2018-01-18 01:33:31 +0530

    using ImageFadeContainerView in activity

--- app/src/main/java/ui/anwesome/com/kotlinimagefadecontainerview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinimagefadecontainerview/MainActivity.kt
@@ -1,12 +1,17 @@
 package ui.anwesome.com.kotlinimagefadecontainerview
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.imagefadecontainerview.ImageFadeContainerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ImageFadeContainerView.create(this,getBitmap(R.drawable.nature),getBitmap(R.drawable.nature1))
     }
+    fun getBitmap(drawable:Int):Bitmap = BitmapFactory.decodeResource(resources,drawable)
 }
+
