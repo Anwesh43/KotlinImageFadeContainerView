ee22e810f3797a86da99a551ac4d622749d2b6d9
Anwesh43 <anweshthecool0@gmail.com>
2018-01-18 00:51:57 +0530

    using state in ImageFadeContainer

--- imagefadecontainerview/src/main/java/ui/anwesome/com/imagefadecontainerview/ImageFadeContainerView.kt
+++ imagefadecontainerview/src/main/java/ui/anwesome/com/imagefadecontainerview/ImageFadeContainerView.kt
@@ -19,6 +19,7 @@ class ImageFadeContainerView(ctx:Context, var bitmap1: Bitmap, var bitmap2:Bitma
         return true
     }
     data class ImageFadeContainer(var bitmap1:Bitmap,var bitmap2:Bitmap, var w:Float, var h:Float) {
+        val state = ImageFadeContainerState()
         init {
             bitmap1 = Bitmap.createScaledBitmap(bitmap1, w.toInt(), w.toInt(), true)
             bitmap2 = Bitmap.createScaledBitmap(bitmap2, w.toInt(), w.toInt(), true)
@@ -27,17 +28,17 @@ class ImageFadeContainerView(ctx:Context, var bitmap1: Bitmap, var bitmap2:Bitma
             canvas.save()
             canvas.translate(w/2,w/2)
             val path = Path()
-            path.addRect(RectF( -w/2, -w/2, -w/2+w, w/2),Path.Direction.CW)
+            path.addRect(RectF( -w/2, -w/2, -w/2+w*state.scale, w/2),Path.Direction.CW)
             canvas.drawBitmap(bitmap1, -w/2, -w/2,paint)
-            path.addRect(RectF(w/2 - w, -w/2, w/2, w),Path.Direction.CW)
+            path.addRect(RectF(w/2 - w*state.scale, -w/2, w/2, w),Path.Direction.CW)
             canvas.drawBitmap(bitmap2, -w/2, -w/2, paint)
             canvas.restore()
         }
         fun update(stopcb:(Float)->Unit) {
-
+            state.update(stopcb)
         }
         fun startUpdating(startcb:()->Unit) {
-
+            state.startUpdating(startcb)
         }
     }
     data class ImageFadeContainerState(var scale:Float = 0f,var dir:Float = 0f,var prevScale:Float = 0f) {
