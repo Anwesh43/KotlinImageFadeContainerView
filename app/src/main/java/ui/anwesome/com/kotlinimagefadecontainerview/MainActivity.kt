3f5f47fe21bc9df4a8abb6f17209ba5fca4cfe89
Anwesh43 <anweshthecool0@gmail.com>
2018-01-18 09:44:36 +0530

    using ImageFadeContainerListener in app

--- app/src/main/java/ui/anwesome/com/kotlinimagefadecontainerview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinimagefadecontainerview/MainActivity.kt
@@ -4,13 +4,19 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import ui.anwesome.com.imagefadecontainerview.ImageFadeContainerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ImageFadeContainerView.create(this,getBitmap(R.drawable.nature),getBitmap(R.drawable.nature1))
+        val view = ImageFadeContainerView.create(this,getBitmap(R.drawable.nature),getBitmap(R.drawable.nature1))
+        view.addImageFadeContainerListener({
+            Toast.makeText(this,"Image1 is shown",Toast.LENGTH_SHORT).show()
+        },{
+            Toast.makeText(this,"Image2 is shown",Toast.LENGTH_SHORT).show()
+        })
     }
     fun getBitmap(drawable:Int):Bitmap = BitmapFactory.decodeResource(resources,drawable)
 }
